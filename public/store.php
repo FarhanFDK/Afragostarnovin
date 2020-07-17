3c48d69dec90ade5e4fd1a5b63eff4fbda63c8ed
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-07-17 17:30:57 +0430

    Store changed

--- public/store.php
+++ public/store.php
@@ -17,7 +17,6 @@
     <link rel="stylesheet" href="../src/css/index.css" />
     <link rel="stylesheet" href="../src/css/hamburger.css" />
     <link rel="stylesheet" href="../src/css/media.css" />
-    <link rel="stylesheet" href="../src/css/store.css" />
     <link
       rel="stylesheet"
       href="../src/assets/slideshow/OwlCarousel/dist/assets/owl.carousel.min.css"
@@ -53,21 +52,21 @@
           <button
             title="صفحه اصلی"
             class="btn text-white font-bold mx-4 my-2 focus:outline-none"
-            href="#"
+            onclick="location.href='index.php'"
           >
             صفحه اصلی
           </button>
           <button
             title="فروشگاه"
             class="btn text-white font-bold mx-4 my-2 focus:outline-none"
-            href="index.html"
+            onclick="location.href='store.php'"
           >
             فروشگاه
           </button>
           <button
             title="مشاوره رایگان"
             class="btn text-white font-bold mx-4 my-2 focus:outline-none"
-            href="store.html"
+            onclick="location.href='free-consulting.php'"
           >
             مشاوره رایگان
           </button>
@@ -88,7 +87,11 @@
         </div>
         <div class="header-content flex-1">
           <p class="text-company m-auto">
-            شرکت افراگستر نوین
+            <a class="cursor-pointer mx-5" title="" href='index.php'>
+              <h1 class="my-3">
+                 شرکت افراگستر نوین
+              </h1>
+            </a>
           </p>
           <p class="phone my-auto mx-2">
             <i class="fa fa-phone"></i>
@@ -106,40 +109,41 @@
         </div>
       </div>
       <div class="container my-16">
-        <div class="store flex flex-row text-center">
-          <div class="store-1">
-            <div class="img-store mx-20 my-10">
-                <img src="../src/Images/store/PAXS90.jpg"/>
-                <div class="text-center">دستگاه کارتخوان مدل S90</div>
-            </div>
-            <div class="img-store mx-20 my-10">
-                <img src="../src/Images/store/PAXS900.png"/>
-                <div class="text-center">دستگاه کارتخوان مدل S900</div>
-            </div>
-          </div>
-          <div class="store-2">
-              <div class="img-store mx-20 my-10">
-                  <img src="../src/Images/store/PAXS910-1.jpg"/>
-                  <div class="text-center">دستگاه کارتخوان مدل S910</div>
-              </div>
-              <div class="img-store mx-20 my-10">
-                <img src="../src/Images/store/IMG-20200622-WA0021.jpg"/>
-                <div class="text-center">دستگاه خودپرداز بیت کوین</div>
-              </div>
-          </div>
-          <div class="store-3">
-            <div class="img-store mx-20 my-10">
-                <img src="../src/Images/store/PAXD210B-1.jpg"/>
-                <div class="text-center">دستگاه کارتخوان مدل D210</div>
-            </div>
-            <div class="img-store mx-20 my-10">
-                <img src="../src/Images/store/PAXS80.jpg"/>
-                <div class="text-center">دستگاه کارتخوان مدل S80</div>
-            </div>
+        <div class="flex flex-row">
+          <div>
+            
           </div>
         </div>
+        <div class="attributes my-5">
+          <div class="text-2xl text-center">
+            ویژگی های محصولات ما:
+          </div>
+          <table class="text-center">
+            <tr class="text-center mx-5">
+              <td class="att-item bg-gray-200 mx-10 text-2xl text-center">
+                عدم بلوکه شدن پول در حساب بانکی
+                <i class="fa fa-line-chart" style="color:rgb(10,20,50)" aria-hidden="true"></i>
+              </td>
+              <td class="att-item bg-gray-200 mx-10 text-2xl text-center">
+                سرعت بالای انجام تراکنش
+                <i class="fa fa-plane fa-rotate-270" style="color:rgb(10,20,50)" aria-hidden="true"></i>
+              </td>
+            </tr>
+            <tr class="text-center mx-5">
+              <td class="att-item bg-gray-200 mx-10 text-2xl text-center">
+                پشتیبانی از تمامی بانک ها
+                <i class="fa fa-credit-card" style="color:rgb(10,20,50)" aria-hidden="true"></i>
+              </td>
+              <td class="att-item bg-gray-200 mx-10 text-2xl text-center">
+
+                <i class="fa fa-" style="color:rgb(10,20,50)" aria-hidden="true"></i>
+              </td>
+            </tr>
+          </table>
+        </div>
         <div class="contact-us flex flex-row">
-          <div class="my-5 mx-5 text-3xl">
+          <div class="my-3 mx-10 text-3xl" style="width:50%;">
+            <br>
             <p>
               مشاوره رایگان دریافت کنید!
               <br>
@@ -150,25 +154,160 @@
                <br>
                ما با شما تماس خواهیم گرفت
             </p>
+            <p>
+              <br>
+              <br>
+              در شبکه های اجتماعی با ما در ارتباط باشید!
+            </p>
+            <div class="social-networks">
+            <a class="" title="" href="#">
+              <i class="fa fa-instagram fa-3x mx-5" aria-hidden="true"></i>
+            </a>
+            <a class="" title="" href="#">
+              <i class="fa fa-telegram fa-3x" aria-hidden="true"></i>
+            </a>
+            <a class="" title="" href="#">
+              <i class="fa fa-whatsapp fa-3x" aria-hidden="true"></i>
+            </a>
+        </div>
           </div>
-          <div class="my-10 mx-10 text-3xl">
-            <label for="firstName">نام:</label>
-            <input class="border-solid border-4 border-gray-600" type="text" name="firstName" id="firstName" required/>
-            <br>
-            <label class="my-5" for="lastName">نام خانوادگی:</label>
-            <input class="mx-5 my-5 border-solid border-4 border-gray-600" type="text" name="lastName" id="lastName" required/>
-            <br>
-            <label class="my-5" for="contact-us-description">توضیحات:</label>
-            <textarea class="text-area-description mx-5 border-solid border-4 border-gray-600" type="text" name="contact-us-description" id="contact-us-description" required></textarea>
-            <br>
-            <input class="" type="submit" name="submit" id="submit" value="ارسال"/>
-            <br>
-            
+          <div class="div-form my-2 mx-10 text-3xl" style="width:50%;">
+            <form class="bg-white rounded px-8 pt-6 pb-8 mb-4" action="index.php" method="post">
+              <label class="my-5" for="firstname">نام:</label>
+              <input class="my-5 firstname border-solid border-4 border-gray-600 shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" name="firstname" id="firstname" required/>
+              <br>
+              <label class="my-5" for="lastname">نام خانوادگی:</label>
+              <input class="my-5 border-solid border-4 border-gray-600 shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" name="lastname" id="lastname" required/>
+              <br>
+              <label class="my-5" for="phonenumber">شماره تماس:</label>
+              <input class="my-5 border-solid border-4 border-gray-600 shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" name="phonenumber" id="phonenumber" required/>
+              <br>
+              <div class="div-description">
+                <div class="inner"> 
+                  <label class="" for="contact-us-description">توضیحات:</label>
+                </div>
+                <textarea class="text-area-description  border-solid border-4 border-gray-600 shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" type="text" name="contact-us-description" id="contact-us-description" required></textarea>
+               </div>
+               
+              
+              <br>
+              <input class="bg-red-700 hover:bg-red-500 text-white font-bold py-2 px-4 rounded cursor-pointer" type="submit" name="submit" id="submit" value="ارسال"/>
+              <br>
+            </form>
+            <div>
+            <?php
+              if(isset($_POST['submit'])){
+                $firstname = $_POST['firstname'];
+                $lastname = $_POST['lastname'];
+                $phonenumber = $_POST['phonenumber'];
+                $description = $_POST['contact-us-description'];
+                date_default_timezone_set("Iran");
+                $date = date("Y/m/d.h:i:sa");
+                $id = $date;
+                $connection = mysqli_connect('localhost', 'admin', '', 'consulting');
+                $chng = mysqli_set_charset($connection, "UTF-8");
+                if($firstname && $lastname && $phonenumber && $description){
+                  $sql = "INSERT INTO users(id,firstname,lastname,phonenumber,descriptions)";
+                  $sql .= "VALUES('$id','$firstname','$lastname','$phonenumber','$description')";
+                  $result = mysqli_query($connection,$sql);
+                  if($result){
+                    $to = "info@afragostarnovin.ir";
+                    $subject = "ایمیل دریافت شده برای مشاوره";
+                    $message = "
+                                <!DOCTYPE html>
+                                <html>
+                                <head>
+                                  <meta charset='UTF-8'/>
+                                  <title>دریافت مشاوره</title>
+                                  <link rel='stylesheet' href='https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css'/>
+                                </head>";
+                    $message .= "<body style='direction:rtl;'>";
+                    $message .= "<div class='header bg-red-700' style='left:0;right:0;top:0;position:absolute;width:100%;height:100px;color:blue;'>
+                                  این ایمیل به منظور دریافت مشاوره رایگان فرستاده شده است،چناچه وقتتان اجازه می دهد،لطفا رسیدگی کنید
+                                </div>
+                                نام:
+                                ";
+                    $message .= $firstname;
+                    $message .= "<br>";
+                    $message .= "نام خانوادگی:";
+                    $message .= $lastname;
+                    $message .= "<br>";
+                    $message .= "شماره تماس:";
+                    $message .= $phonenumber;
+                    $message .= "<br>";
+                    $message .= "توضیحات:";
+                    $message .= $description;
+                    $message .= "زمان ثبت درخواست: ";
+                    $message .= $id;
+                    $message .= "
+                                </body>
+                                </html>
+                    " ;
+                    $headers = "MIME-Version: 1.0" . "\r\n";
+                    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+                    $headers .= 'From: <info@afragostarnovin.ir>' . "\r\n";
+                    $headers .= 'Cc: info@afragostarnovin.ir' . "\r\n";
+                    $sendmail = mail($to,$subject,$message,$headers); 
+                    echo "<p class='success-text' style='color:green;'>درخواست شما با موفقیت ثبت شد</p>";
+                  } else{
+                    echo "<p class='warning-text'>درخواست شما انجام نشد لطفا بعدا تلاش کنید</p>";
+                  }
+                } else{
+                  echo "<p class='warning-text'>لطفا فرم را کامل کنید</p>";
+                }
+              }
+            ?>
+            </div>
           </div>
         </div>
       </div>
       
-      <div class="footer"></div>
+      <div class="footer flex flex-row">
+        <div>
+        <p class="phone my-5 mx-2">
+          <i class="fa fa-phone fa-30x"></i>
+          09354107274
+        </p>
+        <p class="email my-5 mx-2">
+          <i class="fa fa-envelope"></i>
+          <a class="email-footer-send" href = "mailto:info@afragostarnovin.ir">
+            info@afragostarnovin.ir
+          </a>
+        </p>
+        </div>
+      
+      <div>
+        <p class="footer-content my-3 mx-10 text-2xl">
+          دسترسی سریع
+        </p>
+        <p class="footer-links">
+          <a class="mx-5 my-auto" title="" href="#">
+            فروشگاه
+          </a>
+          <a class="mx-5 my-auto" title="" href="">
+            مشاوره رایگان
+          </a>
+          <br>
+        </p>
+      </div>
+      <div class="company-description">
+        <div class="my-5 text-lg">
+          <p>
+          شرکت افراگستر نوین شرکتی در زمینه فروش دستگاه های خودپرداز
+          (ATM)،
+          دستگاه های کارتخوان
+          (POS)
+          و ارائه خدمات دیگر می باشد
+          </p>
+          <p class="my-3 text-lg">
+          خدماتی نظیر طراحی وبسایت،
+          طراحی لوگو،
+          ساخت وب اپلیکیشن و ...
+          </p>
+          برای سفارش با ما تماس بگیرید
+        </div>
+      </div>
+    </div>
     </div>
   </body>
 </html>

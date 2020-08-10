86fb341e10000a11762e2f2f827cce3388128e60
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-08-10 17:58:07 +0430

    Added robots.txt and admin panel

---
+++ public/Admin/admin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <title>ADMIN | شرکت افراگستر نوین</title>
+        <meta name="robots" content="noindex,nofollow" />
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"/>
+        <link rel="stylesheet" href="../src/css/index.css" />
+        <link rel="icon" href="../src/icons/siteicon.ico" />
+        <link rel="shortcut icon" href="../src/icons/siteicon.ico" />
+        <style></style>
+    </head>
+    <body>
+        <div class="header">
+
+        </div>
+        <div class="container">
+
+        </div>
+        <div class="footer">
+
+        </div>
+    </body>
+</html>

8a17cad8a948c25a520047af6b7b8893fa5df80b
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-08-20 10:12:20 +0430

    Details changed

---
+++ public/sample.php
@@ -0,0 +1,29 @@
+<?php
+    require "../src/includes/Exception.php";
+    require "../src/includes/PHPMailer.php";
+    require "../src/includes/SMTP.php";
+    require "../src/includes/PHPMailerAutoload.php";
+    $mail = new PHPMailer(true);
+    try {
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host       = 'mail.afragostarnovin.ir';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'farhanabdollahi@afragostarnovin.ir';
+        $mail->Password   = 'z?OTBYxRm#wh';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port       = '993';
+
+        $mail->setFrom('Farhanabdollahi@afragostarnovin.ir' , 'Farhanabdollahi');
+        $mail->addAddress('Farhanabdollahiab@gmail.com' , 'Farhanabdollahi');
+        $mail->isHTML(true);
+
+        $mail->Subject = 'TEST';
+        $mail->Body =  '<i>TESTING!</i>';
+        $mail->AltBody = 'TEST!';
+    } catch (Exception $e) {
+       echo "message could not be sent {$mail->ErrorInfo}";
+    }
+    
+
+?>

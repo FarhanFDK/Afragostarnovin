ece71682903551ae115ca6dd0af5af164e83452d
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-08-27 01:26:16 -0400

    Details changed

--- public/sample.php
+++ public/sample.php
@@ -1,25 +1,46 @@
 <?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    
+    require 'vendor/autoload.php';
+
     $mail = new PHPMailer(true);
+
     try {
-        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-        $mail->isSMTP();
-        $mail->Host       = 'mail.afragostarnovin.ir';
-        $mail->SMTPAuth   = true;
-        $mail->Username   = 'farhanabdollahi@afragostarnovin.ir';
-        $mail->Password   = 'z?OTBYxRm#wh';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = '993';
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'mail.afragostarnovin.ir';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'Farhanabdollahi@afargostarnovin.ir';                     // SMTP username
+    $mail->Password   = 'z?OTBYxRm#wh';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 993;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-        $mail->setFrom('Farhanabdollahi@afragostarnovin.ir' , 'Farhanabdollahi');
-        $mail->addAddress('Farhanabdollahiab@gmail.com' , 'Farhanabdollahi');
-        $mail->isHTML(true);
+    //Recipients
+    $mail->setFrom('Farhanabdollahi@afargostarnovin.ir', 'Mailer');
+    $mail->addAddress('Farhanabdollahiab@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addAddress('Farhanabdollahisd@gmail.com');               // Name is optional
+    $mail->addReplyTo('info@example.com', 'Information');
+    $mail->addCC('cc@example.com');
+    $mail->addBCC('bcc@example.com');
 
-        $mail->Subject = 'TEST';
-        $mail->Body =  '<i>TESTING!</i>';
-        $mail->AltBody = 'TEST!';
-    } catch (Exception $e) {
-       echo "message could not be sent {$mail->ErrorInfo}";
-    }
+    // Attachments
+    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
     
 
 ?>

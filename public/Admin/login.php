a75583a7a487d100c31a6146716a05162e9b2a77
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-09-07 22:30:22 -0400

    SMALL CHANGES

--- public/Admin/login.php
+++ public/Admin/login.php
@@ -15,7 +15,7 @@
 <html>
     <head>
         <meta charset="UTF-8" />
-        <title>ADMIN | شرکت افراگستر نوین</title>
+        <title>LOGIN | شرکت افراگستر نوین</title>
         <meta name="keywords" content="" />
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
@@ -37,7 +37,7 @@
             <div class="header flex h-16">
                 <div class="menu-with-hamburger">
                     <div id="hamburger" class="block sm:block md:hidden lg:hidden xl:hidden" style="margin-top: 8px; float: right;">
-                        <span class="opener-span cursor-pointer" onclick="menuOpen()"><img src="../src/icons/hamburger.png" style="width:3rem;height:3rem;" /></span>
+                        <span class="opener-span cursor-pointer" onclick="menuOpen()"><img src="../../src/icons/hamburger.png" style="width:3rem;height:3rem;" /></span>
                     </div>
                     <div class="menu-side-navbar block sm:block md:hidden lg;hidden xl:hidden" id="menu-side-navbar">
                         <a class="closebtn" href="javascript:void(0)" onclick="menuClose()">&times;</a>
@@ -46,7 +46,7 @@
                         <a class="" href="https://www.afragostarnovin.ir/public/index#consult" onclick="menuClose()">مشاوره رایگان</a>
                         <a class="" href="https://www.afragostarnovin.ir/public/index#aboutus" onclick="menuClose()">درباره شرکت</a>
                     </div>
-                    <script src="../src/js/menu-opener.js"></script>
+                    <script src="../../src/js/menu-opener.js"></script>
                 </div>
                 <div class="menu flex-1 my-2 hidden sm:hidden md:flex lg:flex xl:flex">
                     <a title="صفحه اصلی" class="cursor-pointer btn-header text-white font-bold mx-4 my-2 text-l" href="https://www.afragostarnovin.ir/public/index.php">
@@ -62,7 +62,7 @@
                         درباره شرکت
                     </a>
                 </div>
-                <a class="cursor-pointer ml-4 text-center sm:text-center md:text-left text-xl truncate float-left sm:float-left" title="" href='#'>
+                <a class="cursor-pointer ml-4 text-center sm:text-center md:text-left text-xl truncate float-left sm:float-left" title="" href='https://www.afragostarnovin.ir'>
                     <h1 class="my-2 font-bold">
                         شرکت افراگستر نوین
                     </h1>
